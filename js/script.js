72ea0e0a77e6c4455b0c1c35d0989e38c5bb4e39
Vincent Renaldo Jita <daejaeblaq@Vincents-MacBook-Pro.local>
2019-06-02 23:08:15 -0700

    final

--- js/script.js
+++ js/script.js
@@ -5,41 +5,41 @@ var words = ['sunshine', 'car', 'elephant', 'javascript', 'millennial', 'banana'
 //Choose one random word from the words array.
  var word = words[Math.floor(Math.random() * words.length)];
  
- var answerArray = [];
+ var arrAnswer = [];
  for (var i = 0; i < word.length; i++) {
-     answerArray[i] = "_";
+     arrAnswer[i] = "_";
  }
  
  //Create variable to keep track of letters remained to be guessed
- var remainingLetters = word.length;
+ var leftGuessed = word.length;
 
  //Game Loop
- while (remainingLetters > 0) {
+ while (leftGuessed > 0) {
     //Show player the progress
-    alert(answerArray.join(" "));
+    alert(arrAnswer.join(" "));
 
     //Get a guess input from player
-    var guess = prompt("Guess a letter, or click cancel to stop playing.");
-    if (guess === null) {
+    var guessed = prompt("Guess a letter! To exit, click cancel.");
+    if (guessed === null) {
         //Exit loop or game
         break;
         //Makes sure it is a single letter.
-    } else if (guess.length !== 1) {
+    } else if (guessed.length !== 1) {
         alert("Please enter a single letter.");
     } else {
         //Update game state
         for (var a = 0; a < word.length; a++) {
-            if (word[a] = guess;) {
-                answerArray[a] = guess;
-                remainingLetters--;
+            if (word[a] = guessed;) {
+                arrAnswer[a] = guessed;
+                leftGuessed--;
             }
         }
     }
     //End of loop
  }
 
-alert(answerArray.join(" "));
-alert("You won! The answer is " + word + ".");
+alert(arrAnswer.join(" "));
+alert("Congratulations, you won! The answer is " + word + ".");
 
 
 
